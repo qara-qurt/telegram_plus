25079b7275de76c6c83d75a21eb2a4db1657f9da
qara-qurt <serikov.2002.12gmail.com>
2024-08-25 14:24:39 +0500

    feat: added gateway

---
+++ gateway/http_handler.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+
+	"github.com/labstack/echo/v4"
+	pb "github.com/qara-qurt/telegrum_plus/user_service/pkg/gen/user"
+)
+
+type handler struct {
+	user_service pb.UserServiceClient
+}
+
+func NewHandler(client pb.UserServiceClient) *handler {
+	return &handler{
+		user_service: client,
+	}
+}
+
+func (h *handler) InitRoutes(http *echo.Echo) {
+	http.GET("/ping", h.Ping)
+	http.GET("/users", h.GetUsers)
+}
+
+func (h *handler) Ping(c echo.Context) error {
+	return c.String(200, "pong")
+}
+
+func (h *handler) GetUsers(c echo.Context) error {
+	users, err := h.user_service.GetUsers(context.Background(), &pb.GetUsersRequests{
+		Page:  1,
+		Limit: 10,	
+	})
+	if err != nil {
+		return c.String(500, err.Error())
+	}
+	return c.JSON(200, users)
+}
---
+++ gateway/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	"github.com/labstack/echo/v4"
+	pb "github.com/qara-qurt/telegrum_plus/user_service/pkg/gen/user"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	PORT_ADDR = ":3000"
+	GPRC_ADDR = "localhost:8080"
+)
+
+func main() {
+	conn, err := grpc.NewClient(GPRC_ADDR, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to gRPC server: %v", err)
+	}
+	defer conn.Close()
+
+	user_service := pb.NewUserServiceClient(conn)
+
+	e := echo.New()
+	handler := NewHandler(user_service)
+	// Register all routes
+	handler.InitRoutes(e)
+
+	// Start server
+	if err := e.Start(PORT_ADDR); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
+}
